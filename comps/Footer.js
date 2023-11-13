f92b726dc2cbe4d13a780456e658f497c037fdb7
Aldo Berziu <berziu2004@gmail.com>
2023-11-13 19:13:16 +0100

    initial

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>Copyright 2023 Eminemi.</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
--- pages/about.js
+++ pages/about.js
@@ -1,32 +1,27 @@
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-import Head from "next/head";
-
-export default function Home() {
+const About = () => {
   return (
     <>
-    <Head>
-      <title>Home | Ninja List</title>
-      <meta name="keywords" content="ninjas"/>
-    </Head>
+      <Head>
+        <title>Home | Ninja List</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam,
           corporis sapiente odit, harum corrupti incidunt, nobis voluptas
           nesciunt pariatur ad voluptate! Ea vel ullam alias molestiae deserunt
           odit commodi in.
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam,
           corporis sapiente odit, harum corrupti incidunt, nobis voluptas
           nesciunt pariatur ad voluptate! Ea vel ullam alias molestiae deserunt
           odit commodi in.
         </p>
-        <Link href="/ninjas" className={styles.btn}>
-          See Ninja Listing
-        </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
